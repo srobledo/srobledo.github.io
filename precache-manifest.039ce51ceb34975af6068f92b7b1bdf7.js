018aee6fa28608ad1945f0b14fed8492929092b0
sergio robledo <sergiorobledo2k5@gmail.com>
2020-09-09 10:45:39 -0500

    first commit

---
+++ precache-manifest.039ce51ceb34975af6068f92b7b1bdf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017f9422afd942106f4fd93a02fad721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d73cd37a91431db39d8b",
+    "url": "/static/css/main.3d5c2617.chunk.css"
+  },
+  {
+    "revision": "d77dac7cf824aa594204",
+    "url": "/static/js/2.c314d9d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c314d9d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73cd37a91431db39d8b",
+    "url": "/static/js/main.5601d997.chunk.js"
+  },
+  {
+    "revision": "7597eead92760efdf22c",
+    "url": "/static/js/runtime-main.5d7d4569.js"
+  }
+]);
